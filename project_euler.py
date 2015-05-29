035e0ca933a762789281d4ec298c5c0a674a00b3
Justice Lenon <justicelenon@Kelles-MacBook-Air.local>
2015-05-29 14:12:18 -0400

    project_euler.py

--- project_euler.py
+++ project_euler.py
@@ -1,6 +1,11 @@
 ''' List all multiples of 3 and 5 from 0 to 10000 and return the sum'''
 def if_multiple( nat_number ):
     '''Return True if it is a multiple of 3 or 5'''
+    for i in range(1,10):
+    if i % 3 == 0 or i % 5 == 0 :
+        print i, 'True'
+    else:
+        print i, 'False
     return
 
 def apppend_to_arrray( myarray, item_to _append ) :
