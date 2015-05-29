67d955243fd2c06ee640b44a5104d3d123e6a760
Justice Lenon <justicelenon@Kelles-MacBook-Air.local>
2015-05-29 13:20:40 -0400

    project_euler.py

---
+++ project_euler.py
@@ -0,0 +1,11 @@
+''' List all multiples of 3 and 5 from 0 to 10000 and return the sum'''
+def if_multiple( nat_number ):
+    '''Return True if it is a multiple of 3 or 5'''
+    return
+
+def apppend_to_arrray( myarray, item_to _append ) :
+    '''Appends item_to_append to myarray'''
+    return
+def sum_numbers( numbers ) :
+    '''Sum of numers'''
+    return
